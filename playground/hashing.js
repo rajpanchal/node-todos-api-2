729895f7180cf1dc89f9fd22c2787871db8a5d25
rajpanchal <hematkumar.2017@vitstudent.ac.in>
2018-05-18 18:41:50 +0530

    hashed passwords before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,28 @@
 const {SHA256} = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-var id = {
-  id = 10
-};
+var password = "raj";
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// })
 
-var token = jwt.sign(id, "123qwerty");
-console.log(token);
-var decoded = jwt.verify(token, "123qwerty");
-console.log(decoded);
+var hashedPass = "$2a$10$3bMT0om6J/0Z5HCfmM9PEOI8v95gCFPgARZCV8BcSNODPQ3aP0X7i";
+
+bcrypt.compare("raj", hashedPass, (err, res) => {
+  console.log(res);
+})
+
+// var id = {
+//   id = 10
+// };
+//
+// var token = jwt.sign(id, "123qwerty");
+// console.log(token);
+// var decoded = jwt.verify(token, "123qwerty");
+// console.log(decoded);
 // var message = "Rish";
 // var hash = SHA256(message).toString();
 //
